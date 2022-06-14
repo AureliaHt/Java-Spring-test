94c6fa40e6610260435f2b9fc2445b9708470e92
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-06-14 12:54:57 +0200

    first commit java & eclipse test

---
+++ src/com/premierpackage/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.premierpackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("je teste du code Java sur l\'IDE Eclipse");
+	}
+
+}
