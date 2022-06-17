3c92e798d97f03dd1f8518f16912a62bda992318
Aurélia Hemeret <aurelia.hemeret@gmail.com>
2022-06-17 15:42:17 +0200

    method for detecting the key pressed event

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -149,7 +149,8 @@ public class GamePanel extends JPanel implements ActionListener {
 
 	}
 
-	// annotation override pour définir une méthode héritée de la classe parente
+	// annotation override pour définir une méthode héritée de la classe parente.
+	// Override annotation to define
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if(gameIsRunning) {
@@ -163,6 +164,28 @@ public class GamePanel extends JPanel implements ActionListener {
 	public class MyKeyAdapter extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
+			switch(e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				if(direction != 'R') {
+					direction = 'L';
+				}
+				break;
+			case KeyEvent.VK_RIGHT:
+				if(direction != 'L') {
+					direction = 'R';
+				}
+				break;
+			case KeyEvent.VK_UP:
+				if(direction != 'D') {
+					direction = 'U';
+				}
+				break;
+			case KeyEvent.VK_DOWN:
+				if(direction != 'U') {
+					direction = 'D';
+					break;
+				}
+			}
 		}
 	}
 }
